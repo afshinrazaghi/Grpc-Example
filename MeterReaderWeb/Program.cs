a11bfd0b57cd4943190d9141ddb6b9baf4487073
Afshin Razaghi <neo1.afshin@gmail.com>
2023-08-11 10:12:32 +0330

    Generate and use certificates configure kestrel

--- MeterReaderWeb/Program.cs
+++ MeterReaderWeb/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using MeterReaderLib;
 using Microsoft.AspNetCore.Hosting;
@@ -11,18 +12,30 @@ using Microsoft.Extensions.Logging;
 
 namespace MeterReaderWeb
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      CreateHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        var configuration = options.ApplicationServices.GetService<IConfiguration>();
+                        var cert = new X509Certificate2(configuration.GetValue<string>("Certificate:File"), configuration.GetValue<string>("Certificate:Password"));
+
+                        options.ConfigureHttpsDefaults(h =>
+                        {
+                            h.ClientCertificateMode = Microsoft.AspNetCore.Server.Kestrel.Https.ClientCertificateMode.AllowCertificate;
+                            h.CheckCertificateRevocation = false;
+                            h.ServerCertificate = cert;
+                        });
+                    });
+                });
+    }
 }
